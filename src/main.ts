df2ba862f798518e1d99025392a733056588116e
Kyojo Henkanhito <sethfb@gmail.com>
2021-09-30 10:36:48 -0400

    Done

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,37 @@
+import { addFlag, getPlayers } from "isaacscript-common";
+
 export default function main(): void {
-  // Instantiate a new mod object, which grants the ability to add callback functions that
-  // correspond to in-game events
-  const mod = RegisterMod("tlaz", 1);
-
-  // Set a callback function that corresponds to when a new run is started
-  mod.AddCallback(ModCallbacks.MC_POST_GAME_STARTED, postGameStarted);
-
-  // Print an initialization message to the "log.txt" file
-  Isaac.DebugString("tlaz initialized.");
+  const mod = RegisterMod("Tainted Laz Rework", 1);
+  mod.AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, playerDMG);
+  mod.AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, roomClear);
+}
+function playerDMG(entity: Entity) {
+  const player = entity.ToPlayer();
+  if (player === undefined) return;
+  if (
+    player.GetPlayerType() !== PlayerType.PLAYER_LAZARUS_B &&
+    player.GetPlayerType() !== PlayerType.PLAYER_LAZARUS2_B
+  )
+    return;
+  player.UseActiveItem(
+    CollectibleType.COLLECTIBLE_FLIP,
+    addFlag(UseFlag.USE_NOANIM, UseFlag.USE_NOANNOUNCER),
+    -1,
+  );
 }
 
-function postGameStarted() {
-  Isaac.DebugString("Callback triggered: MC_POST_GAME_STARTED");
+function roomClear() {
+  for (const i of getPlayers(true)) {
+    if (
+      i.GetPlayerType() !== PlayerType.PLAYER_LAZARUS_B &&
+      i.GetPlayerType() !== PlayerType.PLAYER_LAZARUS2_B
+    ) {
+      return;
+    }
+    i.UseActiveItem(
+      CollectibleType.COLLECTIBLE_FLIP,
+      addFlag(UseFlag.USE_NOANIM, UseFlag.USE_NOANNOUNCER),
+      -1,
+    );
+  }
 }
