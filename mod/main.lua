a0325422d4c518afdd8f422b30120f7302bdd777
Kyojo <sethfb@gmail.com>
2021-10-25 07:10:56 -0400

    Fixed the speed issue

--- mod/main.lua
+++ mod/main.lua
@@ -2227,7 +2227,24 @@ local ____exports = {}
 local ____isaacscript_2Dcommon = require("lua_modules.isaacscript-common.dist.index")
 local addFlag = ____isaacscript_2Dcommon.addFlag
 local getPlayers = ____isaacscript_2Dcommon.getPlayers
-local playerDMG, roomClear
+local doit, update, playerDMG, roomClear
+function update(self)
+    if doit == 1 then
+        for ____, i in ipairs(
+            getPlayers(nil, true)
+        ) do
+            if (i:GetPlayerType() ~= PlayerType.PLAYER_LAZARUS_B) and (i:GetPlayerType() ~= PlayerType.PLAYER_LAZARUS2_B) then
+                return
+            end
+            i:UseActiveItem(
+                CollectibleType.COLLECTIBLE_FLIP,
+                addFlag(nil, UseFlag.USE_NOANIM, UseFlag.USE_NOANNOUNCER),
+                -1
+            )
+        end
+    end
+    doit = ((doit <= 1) and 0) or (doit - 1)
+end
 function playerDMG(self, entity)
     local player = entity:ToPlayer()
     if player == nil then
@@ -2243,23 +2260,14 @@ function playerDMG(self, entity)
     )
 end
 function roomClear(self)
-    for ____, i in ipairs(
-        getPlayers(nil, true)
-    ) do
-        if (i:GetPlayerType() ~= PlayerType.PLAYER_LAZARUS_B) and (i:GetPlayerType() ~= PlayerType.PLAYER_LAZARUS2_B) then
-            return
-        end
-        i:UseActiveItem(
-            CollectibleType.COLLECTIBLE_FLIP,
-            addFlag(nil, UseFlag.USE_NOANIM, UseFlag.USE_NOANNOUNCER),
-            -1
-        )
-    end
+    doit = 5
 end
+doit = 0
 function ____exports.default(self)
     local mod = RegisterMod("Tainted Laz Rework", 1)
     mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, playerDMG)
     mod:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, roomClear)
+    mod:AddCallback(ModCallbacks.MC_POST_UPDATE, update)
 end
 return ____exports
  end,
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { addFlag, getPlayers } from "isaacscript-common";
 
+let doit = 0;
 export default function main(): void {
   const mod = RegisterMod("Tainted Laz Rework", 1);
   mod.AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, playerDMG);
   mod.AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, roomClear);
+  mod.AddCallback(ModCallbacks.MC_POST_UPDATE, update);
+}
+function update() {
+  if (doit === 1) {
+    for (const i of getPlayers(true)) {
+      if (
+        i.GetPlayerType() !== PlayerType.PLAYER_LAZARUS_B &&
+        i.GetPlayerType() !== PlayerType.PLAYER_LAZARUS2_B
+      ) {
+        return;
+      }
+      i.UseActiveItem(
+        CollectibleType.COLLECTIBLE_FLIP,
+        addFlag(UseFlag.USE_NOANIM, UseFlag.USE_NOANNOUNCER),
+        -1,
+      );
+    }
+  }
+  doit = doit <= 1 ? 0 : doit - 1;
 }
 function playerDMG(entity: Entity) {
   const player = entity.ToPlayer();
@@ -21,17 +41,5 @@ function playerDMG(entity: Entity) {
 }
 
 function roomClear() {
-  for (const i of getPlayers(true)) {
-    if (
-      i.GetPlayerType() !== PlayerType.PLAYER_LAZARUS_B &&
-      i.GetPlayerType() !== PlayerType.PLAYER_LAZARUS2_B
-    ) {
-      return;
-    }
-    i.UseActiveItem(
-      CollectibleType.COLLECTIBLE_FLIP,
-      addFlag(UseFlag.USE_NOANIM, UseFlag.USE_NOANNOUNCER),
-      -1,
-    );
-  }
+  doit = 5;
 }
